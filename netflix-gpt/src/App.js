11bced8ff55cd0d86ae2b63c730101332e6f061e
PradyumnaChippigiri <prch4538@colorado.edu>
2025-02-04 12:33:03 +0530

    first commit

---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p className= "text-3xl font-bold p-2 m-2"> Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
